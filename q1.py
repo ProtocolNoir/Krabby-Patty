a84b57f26d61c25be81dab1b341689f7909ea96b
Harsh Thakur <harshthakur9876@gmail.com>
2020-09-26 23:16:16 +0530

    DONE

--- q1.py
+++ q1.py
@@ -12,25 +12,53 @@ for a,b in d.items():
     print(a,b, end=' ')
 print(" ")
 
+tally = []
+tempdict = OrderedDict()
+tally = d.keys()
+for a in tally:
+    t = tempdict.get(d[a],0) + 1
+    tempdict[d[a]] = t
+
 d1 = d.copy()
 flag = 0
 for a in freq:
-    for i,j in d.items():
-        if j == a and flag == 0:
-            flag = 1
-        elif j == a and flag == 1:
-            flag = 0
-        elif flag == 1:
-            if j<a:
-                d1.move_to_end(i, last=False)
-            elif j>a:
-                d1.move_to_end(i)
-            elif j == a:
+    t = tempdict[a]
+    if t%2 == 0:
+        for i,j in d.items():
+            if j == a and flag == 0:
+                flag = 1
+            elif j == a and flag == 1:
                 flag = 0
-    for a,b in d1.items():
-        print(a,b, end=' ')
-    print(" ")
-    d.clear()
-    d = d1.copy()
-    flag = 0
+            elif flag == 1:
+                if j<a:
+                    d1.move_to_end(i, last=False)
+                elif j>a:
+                    d1.move_to_end(i)
+        for a,b in d1.items():
+            print(a,b, end=' ')
+        print(" ")
+        d.clear()
+        d = d1.copy()
+        flag = 0
+    else:
+        check = 0
+        for i,j in d.items():
+            if j == a and flag == 0:
+                if check != t-1:
+                    flag = 1
+                    check = check +1
+            elif j == a and flag == 1:
+                flag = 0
+                check = check +1
+            elif flag == 1:
+                if j<a:
+                    d1.move_to_end(i, last=False)
+                elif j>a:
+                    d1.move_to_end(i)
+        for a,b in d1.items():
+            print(a,b, end=' ')
+        print(" ")
+        d.clear()
+        d = d1.copy()
+        flag = 0
 
