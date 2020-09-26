68614f3f63e56f1b9ea5f46bbb08a2aefd377fa4
Harsh Thakur <harshthakur9876@gmail.com>
2020-09-26 17:49:50 +0530

    00.00.0.0.0.0.0.0.0.

--- q1.py
+++ q1.py
@@ -1,5 +1,7 @@
 from collections import OrderedDict 
+import numpy as np
 d = OrderedDict()
+d1 = OrderedDict()
 str = "nomatterhowbusyyoumaythinkyouareyoumustfindtimeforreadingorsurrenderyourselftoselfchosenignorance"
 for a in str:
     if a not in d:
@@ -8,4 +10,13 @@ for a in str:
         d[a] = d[a] + 1
 for a,b in d.items():
     print(a,b, end=' ')
+print(" ")
+arr1 = []
+arr1 = d.keys()
+'''arr2 = np.array(d.values())'''
+for a in arr1:
+    b = d[a]
+    d1[b] = d1.get(b,0) + 1
+for a,b in d1.items():
+    print(a,b)
 print(" ")
