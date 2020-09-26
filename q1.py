a6109b10c26bdf46246474a93316e074bae5d37c
Harsh Thakur <harshthakur9876@gmail.com>
2020-09-26 22:56:35 +0530

    almost done

--- q1.py
+++ q1.py
@@ -12,9 +12,25 @@ for a,b in d.items():
     print(a,b, end=' ')
 print(" ")
 
-d1 = []
+d1 = d.copy()
+flag = 0
 for a in freq:
     for i,j in d.items():
-        if j == a:
-            break
-    
+        if j == a and flag == 0:
+            flag = 1
+        elif j == a and flag == 1:
+            flag = 0
+        elif flag == 1:
+            if j<a:
+                d1.move_to_end(i, last=False)
+            elif j>a:
+                d1.move_to_end(i)
+            elif j == a:
+                flag = 0
+    for a,b in d1.items():
+        print(a,b, end=' ')
+    print(" ")
+    d.clear()
+    d = d1.copy()
+    flag = 0
+
