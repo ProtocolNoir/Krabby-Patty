6474c82530e109250403a8fc8a663f56916012a2
Harsh Thakur <harshthakur9876@gmail.com>
2020-09-26 22:31:47 +0530

    0.0

--- q1.py
+++ q1.py
@@ -1,8 +1,8 @@
 from collections import OrderedDict 
-import numpy as np
 d = OrderedDict()
-d1 = OrderedDict()
-str = "nomatterhowbusyyoumaythinkyouareyoumustfindtimeforreadingorsurrenderyourselftoselfchosenignorance"
+str = input()
+n = int(input())
+freq = list(map(int,input().strip().split()))[:n] 
 for a in str:
     if a not in d:
         d[a] = 1
@@ -11,12 +11,10 @@ for a in str:
 for a,b in d.items():
     print(a,b, end=' ')
 print(" ")
-arr1 = []
-arr1 = d.keys()
-'''arr2 = np.array(d.values())'''
-for a in arr1:
-    b = d[a]
-    d1[b] = d1.get(b,0) + 1
-for a,b in d1.items():
-    print(a,b)
-print(" ")
+
+d1 = []
+for a in freq:
+    for i,j in d.items():
+        if j == a:
+            break
+    
